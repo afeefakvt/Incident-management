e1feee95f3138983e226ef31718f35261a2c29a5
afeefakvt <afeefakvt6@gmail.com>
2025-08-31 21:17:21 +0530

    fix: type issues

--- incidents-app/lib/queries/users.ts
+++ incidents-app/lib/queries/users.ts
@@ -1,4 +1,4 @@
-import { useQuery,useMutation,useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { apiClient } from "../api-client";
 import { queryKeys } from "../query-keys";
 
